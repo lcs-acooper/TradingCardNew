c734ad2097d972c4547b9f9326c71fcca06debf9
AJ Cooper <acooper@lcs.on.ca>
2024-01-22 08:55:24 -0500

    Initial Commit

---
+++ TradingCardNew/TradingCardNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardNewApp.swift
+//  TradingCardNew
+//
+//  Created by  Adam-James  Cooper on 2024-01-22.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
